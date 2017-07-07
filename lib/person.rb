01244b0b33f9c4be13237325ff6901f962a0ae52
github username <github email address>
2017-07-06 23:08:40 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,24 @@
 class Person
   #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
+:weight, :handed, :complexion, :t_shirt_size, 
+:wrist_size, :glove_size, :pant_length, :pant_width
+
+	# ATTRIBUTES = {
+	# 	name:, birthday:, hair_color:, eye_color:, height:, 
+	# 	weight:, handed:, complexion:, t_shirt_size:, 
+	# 	wrist_size:, glove_size:, pant_length:, pant_width:
+	# }
+
+  def initialize(attributes = {})
+  	# create a loop with .send to assign all the attributes in the hash
+  	attributes.each do |key, value| 
+  		self.send("#{key}=", value)
+  	end
+  end
+
+
+
+
+
 end
